ceead4155b945445d43b6e1a672d2f2e18ef87c4
mike jordan <mike_jordan79@yahoo.com>
2022-06-30 18:07:31 -0400

    set up first step

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,37 @@
 // Assignment code here
+function makepassword() {
+  var numbers= "123456789";
 
+  var uppercase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  var lowercase= "abcdefghijklmnopqrstuvwxyz";
+
+  var special characters= "!@#$%^&*()";
+
+  var randomized password ="";
+
+  var x = Math.floor(Math.random());
+
+  var rand = numbers[x];
+
+  var rand = uppercase[x];
+
+  var rand = lowercase [x];
+  
+  var rand = special characters[x];
+
+  alert (rand);
+
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -15,3 +41,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
